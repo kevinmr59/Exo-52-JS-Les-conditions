6c66a49f8476a334d2401506e68d26795ca508ae
kevinmr59 <kevinmura59610@gmail.com>
2021-11-10 15:35:24 +0100

    fini

--- exercice 1/exercice1.js
+++ exercice 1/exercice1.js
@@ -1,6 +1,7 @@
 /* Modifier le code ci dessous pour réaliser l'exercice */
-
-if x > y
+let x=2
+let y=1
+if (x > y)
     document.getElementById('monDiv').innerHTML = "La variable x est supérieure à la variable y";
 
 
--- exercice3/exercice3.js
+++ exercice3/exercice3.js
@@ -1,10 +1,15 @@
 /* Modifier le code ci dessous pour réaliser l'exercice */
-
-if()
+let x=5;
+let y=x++;
+let y=5;
+if(x===y)
 {
-
+    document.getElementById('monDiv').innerHTML = "X est strictement égal a y";
+    alert("x est strictement égal a y")
 }
 
+else
 {
-
+    document.getElementById('monDiv').innerHTML = "";
+    alert(document)
 }
